6295ec10cd40a5a8310bb4cc1cecb40ad794842e
_mthom <matt@mthompson.org>
2017-10-26 09:59:30 +0100

    Initial setup of project structure

---
+++ test/tableschema_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns tableschema-clj.core-test
+  (:require [clojure.test :refer :all]
+            [tableschema-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
