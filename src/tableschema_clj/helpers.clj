a9b51a46035e11b47fd44ec4ab1b6ac3a30feab9
_mthom <matt@mthompson.org>
2017-11-12 11:08:41 +0000

    Fixed helpers

--- src/tableschema_clj/helpers.clj
+++ src/tableschema_clj/helpers.clj
@@ -1,23 +1,2 @@
-(ns tableschema-clj.helpers
-  (:require [tableschema-clj.types :refer :all]))
-
-(def type-lookup
-  {:any ::any
-   :array ::array
-   :base ::base
-   :boolean ::boolean
-   :date ::date
-   :datetime ::datetime
-   :geojson ::geojson
-   :geopoint ::geopoint
-   :integer ::integer
-   :number ::number
-   :object ::object
-   :string ::string
-   :time ::time
-   :year ::year
-   :yearmonth ::yearmonth
-   :duration ::duration})
-
-(defn get-type [])
+(ns tableschema-clj.helpers)
 
